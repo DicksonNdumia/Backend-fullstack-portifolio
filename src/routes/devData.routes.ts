c3e3943c440c40a4019bcb06e3127efd3b1291cf
DicksonNdumia <ndumiadickson19@gmail.com>
2026-06-04 14:03:48 +0300

    refactor: simplify controllers using custom errors

--- src/routes/devData.routes.ts
+++ src/routes/devData.routes.ts
@@ -1,12 +1,10 @@
-import express from "express";
-import {protect} from "../middleware/auth/protect.ts";
-import {devDetails, getDevDetails} from "../controller/devData.controller.ts";
-
-
+import express from 'express'
+import { protect } from '../middleware/auth/protect.ts'
+import { devDetails, getDevDetails } from '../controller/devData.controller.ts'
 
 const router = express.Router()
 
-router.post('/add/dev/:userId',protect,devDetails)
-router.get('/details/:id',getDevDetails)
+router.post('/add/dev/:userId', protect, devDetails)
+router.get('/details/:id', getDevDetails)
 
-export  default router
+export default router
